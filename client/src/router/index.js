4c05d66ffcbf6e9de79ea31906a961773088835c
Soumya Gupta <166936873+soumya27062005@users.noreply.github.com>
2026-05-24 23:17:50 +0530

    Add router index file for common and layout routes

---
+++ client/src/router/index.js
@@ -0,0 +1,10 @@
+// Common Routes
+export { Loader } from "../components/common/Loader";
+export { Search } from "../components/Search";
+export { CategoryDropDown } from "../components/common/CategoryDropDown";
+export { Title, Body, Caption, CustomLink, CustomNavLink, Container, PrimaryButton, ProfileCard, Heading, CustomNavLinkList } from "../components/common/Design";
+
+// Layout Routes
+export { DashboardLayout } from "../components/common/layout/DashboardLayout";
+export { Layout } from "../components/common/layout/Layout";
+
