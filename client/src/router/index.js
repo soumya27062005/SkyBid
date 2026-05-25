d216a02fba3144dd35b728c0f819dab2449a762c
Soumya Gupta <166936873+soumya27062005@users.noreply.github.com>
2026-05-25 22:42:45 +0530

    Refactor route exports in index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,3 +1,44 @@
+export { PrivateRoute } from "./PrivateRoute";
+export { ScrollToTop } from "../utils/ScrollToTop";
+export { NotFound } from "../components/common/NotFound";
+export { WinningBidList } from "../screens/product/WinningBidList";
+export { UserList } from "../admin/UserList";
+
+// Home Section
+export { CategoryCard } from "../components/cards/CategoryCard";
+export { MyFavorite } from "../components/cards/MyFavorite";
+export { CategorySlider } from "../components/hero/CategorySlider";
+export { Hero } from "../components/hero/Hero";
+export { Process } from "../components/hero/Process";
+export { TopCollection } from "../components/hero/TopCollection";
+export { TopSeller } from "../components/hero/TopSeller";
+export { Trust } from "../components/hero/Trust";
+export { Home } from "../screens/home/Home";
+export { Contact } from "../screens/home/Contact";
+export { Blog } from "../screens/home/Blog";
+export { Services } from "../screens/home/Services";
+export { About } from "../screens/home/About";
+
+// Admin Product Routes
+export { Dashboard } from "../screens/dashboard/Dashboard";
+export { AdminProductList } from "../admin/product/AdminProductList";
+export { UpdateProductByAdmin } from "../admin/product/UpdateProductByAdmin";
+export { Income } from "../admin/Income";
+
+// Category Routes
+export { CreateCategory } from "../admin/category/CreateCategory";
+export { UpdateCategory } from "../admin/category/UpdateCategory";
+export { Catgeorylist } from "../admin/category/Catgeorylist";
+
+// Product Routes
+export { ProductsDetailsPage } from "../screens/product/ProductsDetailsPage";
+export { ProductList } from "../screens/product/productlist/ProductList";
+export { ProductEdit } from "../screens/product/ProductEdit";
+export { AddProduct } from "../screens/product/AddProject";
+
+// Utils Routes
+export { DateFormatter } from "../utils/DateFormatter";
+
 // Common Routes
 export { Loader } from "../components/common/Loader";
 export { Search } from "../components/Search";
@@ -8,3 +49,11 @@ export { Title, Body, Caption, CustomLink, CustomNavLink, Container, PrimaryButt
 export { DashboardLayout } from "../components/common/layout/DashboardLayout";
 export { Layout } from "../components/common/layout/Layout";
 
+// Auth Routes
+export { Register } from "../screens/auth/Register";
+export { Login } from "../screens/auth/Login";
+export { LoginAsSeller } from "../screens/auth/LoginAsSeller";
+export { UserProfile } from "../screens/auth/UserProfile";
+export { SellerDashboard } from "../pages/SellerDashboard";
+export { Payment } from "../payment/Payment";
+export { Account } from "../payment/Account";
